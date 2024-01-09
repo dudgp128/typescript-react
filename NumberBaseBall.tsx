af4833b2026f86c15c985c0fab91c6349f6abde4
dudgp128 <nbv19@naver.com>
2024-01-09 17:57:58 +0900

    게임 로직 완성하기

--- NumberBaseBall.tsx
+++ NumberBaseBall.tsx
@@ -2,30 +2,72 @@ import React, { useCallback, useRef, useState } from "react";
 import { Try } from "./Try";
 import { TryInfo } from "./types";
 
+const getNumbers = () => {
+  const candidates = Array.from({ length: 9 }, (v, i) => i + 1);
+  const array = [];
+  for (let i = 0; i < 4; i++) {
+    const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+};
+
 const NumberBaseBall = () => {
-  const [value, setValue] = useState(
-    Array.from(length :4, () => Math.ceil(Math.random() * 9))
-  );
-  const [answer, setAnswer] = useState("");
+  const [answer, setAnswer] = useState(getNumbers());
+  const [value, setValue] = useState("");
   const [result, setResult] = useState("");
-  const [count, setCount] = useState(0);
   const [tries, setTries] = useState<TryInfo[]>([]);
 
   const inputEL = useRef<HTMLInputElement>(null);
 
-  const onSubmit = () => {
-    const array=answer.split('');
-     tries.push({ try: count, result: result });
+  const getResult = () => {
+    if (value == answer.join("")) {
+      setTries((t) => [...t, { try: value, result: "홈런!" }]);
+      setResult("홈런!");
+    } else {
+      const answerArray = value.split("").map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+
+      if (tries.length >= 9) {
+        setResult("실패");
+      } else {
+        console.log(`답은 ${answer.join("")}`);
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === answer[i]) {
+            strike++;
+          } else if (answer.includes(answerArray[i])) {
+            ball++;
+          }
+        }
+        setTries((t) => [
+          ...t,
+          { try: value, result: `${strike} 스트라이크, ${ball} 볼` },
+        ]);
+      }
+    }
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const input = inputEL.current;
+    getResult();
+    if (result === "홈런!" || "실패") {
+      setAnswer(getNumbers());
+      setTries([]);
+    }
+    setValue("");
+    input?.focus();
   };
 
   const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setAnswer(e.target.value);
+    setValue(e.target.value);
   }, []);
 
   return (
     <>
       <form onSubmit={onSubmit}>
-        <input ref={inputEL} onChange={onChange} value={answer} />
+        <input ref={inputEL} onChange={onChange} value={value} />
         <h3> 시도 : </h3>
         <Try tryinfo={tries} />
       </form>
--- types.tsx
+++ types.tsx
@@ -1,4 +1,4 @@
 export interface TryInfo {
-  try: number;
+  try: string;
   result: string;
 }
